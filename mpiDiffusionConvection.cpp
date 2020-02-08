dc3353a99cbc0df631e90f6f5b927c57f48faa98
omarkahol <okahol@yahoo.it>
2020-02-08 09:29:46 +0100

    ottimizzato thread...cpp

--- mpiDiffusionConvection.cpp
+++ mpiDiffusionConvection.cpp
@@ -7,12 +7,13 @@
 #include <chrono>
 
 // VARIABILI DEL PROBLEMA
-const int N_POINTS_PROCESS =20;
-const double LENGTH = 1;
-const int ITERATIONS_TIME=1000;
-const double C = 5;
+const int N_POINTS_PROCESS =20; //NUMERO DI PUNTI DA ASSEGNARE AD OGNI PROCESSO
+const double LENGTH = 1;  //LUNGHEZZA DELL'INTERVALLO
+const int ITERATIONS_TIME=1000; //ITERAZIONI TEMPORALI
+const double C = 5;   
 const double K = 0.001;
 
+//FUNZIONE PER SALVARE I DATI SU UN FILE DI TESTO CSV
 void writeData (int npoints, const std::vector<double>* storage, std::string&& name){
     std::fstream f;
     f.open(name,std::ios::out);
@@ -28,6 +29,7 @@ void writeData (int npoints, const std::vector<double>* storage, std::string&& n
     f.close();
 }
 
+//METODO MAIN
 int main(int argc, char* argv[]) {
 
     //INIZIALIZZO MPI
@@ -35,43 +37,46 @@ int main(int argc, char* argv[]) {
     ierr = MPI_Init(&argc, &argv); // INIZIALIZZA LE VARIABILI DI MPI
     ierr = MPI_Comm_rank(MPI_COMM_WORLD,&procID); //DETERMINA L'ID DEL PROCESSO
     ierr = MPI_Comm_size(MPI_COMM_WORLD, &numProcs); // DETERMINA IL NUMERO DI PROCESSI
-    MPI_Status status;
+    MPI_Status status; // STATO DEL PROCESSO
 
-    const int N_POINTS_TOTAL = numProcs*N_POINTS_PROCESS - 2*numProcs;
-    const double DX = LENGTH/(N_POINTS_TOTAL-1);
+    const int N_POINTS_TOTAL = numProcs*N_POINTS_PROCESS - 2*numProcs; // CALCOLO IL NUMERO DI PUNTI TOTALI DELLA GRIGLIA
+    const double DX = LENGTH/(N_POINTS_TOTAL-1); 
     const double sigma = 0.1;
-    const double DT = sigma*DX;
+    const double DT = sigma*DX;  // CALCOLO DELLO STEP TEMPORALE
 
     if (procID==0) {
-        std::cout << "Solving for nPOINTS = " << N_POINTS_TOTAL << std::endl;
+        std::cout << "Solving for nPOINTS = " << N_POINTS_TOTAL << std::endl; //STAMPA IL NUMERO DI PUNTI TOTALE
     }
 
+    //CREO LA GRIGLIA DI CUI SI DOVRA' OCCUPARE OGNI PROCESSO
     double processGrid[N_POINTS_PROCESS];
-    double phase = procID*(N_POINTS_PROCESS-2)*DX;
+    double phase = procID*(N_POINTS_PROCESS-2)*DX; //OGNI PROCESSO HA UNO START DVERSO, SFASATO 
 
     for (int i=0; i<N_POINTS_PROCESS; i++){
-        processGrid[i] = phase + i*DX;
+        processGrid[i] = phase + i*DX; //RIEMPIO LA GRIGLIA
     }
-
-    if (procID == numProcs -1) {
+     //CONDIZIONE AL CONTORNO PERIODICA, L'ULTIMO PROCESSO LAVORA ANCHE SUI DUE NODI INIZIALI
+    if (procID == numProcs -1) { 
         processGrid[N_POINTS_PROCESS-2] = 0;
         processGrid[N_POINTS_PROCESS-1] = DX;
     }
 
     //IMPONIAMO LA CONDIZIONE INIZIALE
-    double uOld[N_POINTS_PROCESS];
+    double uOld[N_POINTS_PROCESS];    
     double uNew[N_POINTS_PROCESS];
     for (int i=0; i<N_POINTS_PROCESS; i++) {
         uOld[i]=std::sin(2*M_PI*processGrid[i]/LENGTH);
         uNew[i]=std::sin(2*M_PI*processGrid[i]/LENGTH);
     }
 
-    int it = 0;
-
-    double storage[ITERATIONS_TIME][N_POINTS_PROCESS];
+    //OGNI PROCESSO HA UN ARRAY IN CUI PUÒ SALVARE I RISULTATI DI OGNI ITERAZIONE TEMPORALE
+    double storage[ITERATIONS_TIME][N_POINTS_PROCESS]; 
 
     auto start = std::chrono::high_resolution_clock::now();
-    //EVOLVIAMO NEL TEMPO
+
+    //--------------------------------------------------------------------------------------------------------------
+    //EVOLVIAMO NEL TEMPO --> COMINCIA LA SOLUZIONE
+    int it = 0;
     while (it++<ITERATIONS_TIME) {
 
         //EVOLVO NEI PUNTI DEL DOMINIO
@@ -81,7 +86,7 @@ int main(int argc, char* argv[]) {
         }
 
         //COMUNICO I RISULTATI CON GLI ALTRI PROCESSI
-        // Ricevo il primo nodo, spedisco il secondo e il penultimo e ricevo l'ultimo
+        //NODO 0 INIZIA LA COMUNICAZIONE CON L'ULTIMO NODO E POI CON IL SECONDO
         if (procID == 0) {
             MPI_Send(&uNew[1],1,MPI_DOUBLE,numProcs-1,0,MPI_COMM_WORLD);
             MPI_Recv(&uNew[0],1,MPI_DOUBLE,numProcs-1,0,MPI_COMM_WORLD, &status);
@@ -92,17 +97,19 @@ int main(int argc, char* argv[]) {
             MPI_Send(&uNew[N_POINTS_PROCESS-2],1,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
             MPI_Recv(&uNew[0],1,MPI_DOUBLE,procID-1,0,MPI_COMM_WORLD, &status);
             MPI_Send(&uNew[1],1,MPI_DOUBLE,procID-1,0,MPI_COMM_WORLD);
-        } else {
+        } else { //GLI ALTRI NODI SEGUONO A CASCATA--> VIENE RICEVUTO IL NODO 0, SPEDITI IL NODO 1 E N-2 E POI RICEVUTO L'ULTIMO
             MPI_Recv(&uNew[0],1,MPI_DOUBLE,procID-1,0,MPI_COMM_WORLD, &status);
             MPI_Send(&uNew[1],1,MPI_DOUBLE,procID-1,0,MPI_COMM_WORLD);
             MPI_Send(&uNew[N_POINTS_PROCESS-2],1,MPI_DOUBLE,procID+1,0,MPI_COMM_WORLD);
             MPI_Recv(&uNew[N_POINTS_PROCESS-1],1,MPI_DOUBLE,procID+1,0,MPI_COMM_WORLD, &status);
         }
         for (int i=0; i<N_POINTS_PROCESS; i++) {
-            uOld[i] = uNew[i];
-            storage[it-1][i] = uNew[i];
+            uOld[i] = uNew[i];   //UOLD = UNEW
+            storage[it-1][i] = uNew[i];  //SALVO I RISULTATI NELLO STORAGE
         }
     }
+    //FINE RISOLUZIONE
+    //------------------------------------------------------------------------------------------------------------------------------
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
@@ -111,22 +118,24 @@ int main(int argc, char* argv[]) {
         std::cout << "Elapsed Time: " << duration.count()<< " microseconds" << std::endl;
     }
 
+    // ADESSO IL PROCESSO 0 SI OCCUPA DI RICEVERE TUTTI I DATI DAGLI ALTRI PROCESSI
     if (procID == 0) {
 
-        auto* toDump = new std::vector<double>[ITERATIONS_TIME];
+        auto* toDump = new std::vector<double>[ITERATIONS_TIME]; // ARRAY IN CUI VENGONO SALVATI I RISULTATI
 
-        //START FILLING STORAGE
+        //RIEMPIO IL DATABASE CON I DATI DEL PROCESSO ZERO
         for(int i=0; i<ITERATIONS_TIME; i++) {
             for (int j=0; j<N_POINTS_PROCESS-2; j++) {
                 toDump[i].push_back(storage[i][j]);
             }
         }
 
+        //CHIEDO AGLI ALTRI PROCESSI DI SPEDIRE I DATI
         for (int sender = 1; sender<numProcs; sender++) {
-            double toReceive[ITERATIONS_TIME][N_POINTS_PROCESS];
+            double toReceive[ITERATIONS_TIME][N_POINTS_PROCESS]; // ALLOCARE MEMORIA PER RICEVERE I DATI
             MPI_Recv(&toReceive,ITERATIONS_TIME*N_POINTS_PROCESS,MPI_DOUBLE,sender,0,MPI_COMM_WORLD, &status);
             for(int i=0; i<ITERATIONS_TIME; i++) {
-                for (int j=0; j<N_POINTS_PROCESS-2; j++) {
+                for (int j=0; j<N_POINTS_PROCESS-2; j++) { //GLI ULTIMI DUE ELEMENTI SONO DOPPI
                     toDump[i].push_back(toReceive[i][j]);
                 }
             }
@@ -137,10 +146,11 @@ int main(int argc, char* argv[]) {
         delete[] toDump;
         std::system("python3 postProcessorDiffusionConvection.py");
     } else {
+        //SE IL PROCESSO IN QUESTIONE NON È 0 ALLORA SPEDISCI I DATI AL PROCESSO 0
         MPI_Send(&storage,N_POINTS_PROCESS*ITERATIONS_TIME,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
     }
 
-
+    //FINE
     ierr = MPI_Finalize();
     return 0;
 }
--- threadDiffusionConvection.cpp
+++ threadDiffusionConvection.cpp
@@ -9,94 +9,62 @@
 
 //VARIABILI DEL SISTEMA
 const int NTHREADS = 4; //NUMERO DI THREADS
-const int NPOINTS_THREAD = 20; //NUMERO DI PUNTI PER OGNI THREAD
+const int NPOINTS_THREAD = 21; //NUMERO DI PUNTI PER OGNI THREAD
 const int NPOINTS = NTHREADS*NPOINTS_THREAD - 2*NTHREADS; //NUMERO DI PUNTI TOTALE
 const double LENGTH = 1;
 const double DX = LENGTH/(NPOINTS-1);
-const int ITERATIONS_TIME=500;
+const int ITERATIONS_TIME=1000;
 const double C = 5;
 const double K = .02;
 const double SIGMA = 0.1;
 const double DT = SIGMA*DX;
 
-//VARIABILI PER I THREADS
-std::mutex lockCounter;
-int counterThread = 0;
-bool canExecute[NTHREADS];
+//VARIABILE PER SALVARE IL NUMERO DI ITERAZIONI EFFETTUATE
+long numberExecution = 0;
+std::mutex modifyNumExec;
 
-/*THREAD PER EVOLVERE UOLD E SALVARE I RISULTATI IN UNEW --> ne verranno creati NTHREADS
-Si occupa di evolvere solo della fetta di array [start, end]
+/*
+THREAD
+Ogni thread lavora e aggiorna lo stesso file: dataStorage. Tuttavia ogni thread si occupa di evolvere solo 
+i punti interni al suo dominio assegnato tramite [start, end]. Alla fine modifica la variabile numberExecution
+per avviasare che l'iterazione è stata completata con successo.
 */
-void evolveThread(int ID, int start, int end, double* uNew, const double* uOld) {
+void evolveThread(int start, int end, std::vector<std::vector<double>>& dataStorage) {
 
-    for(int t = 0; t<ITERATIONS_TIME; t++) {
+    for(int t = 0; t<ITERATIONS_TIME-1; t++) {
 
-        // ASPETTA CHE IL THREAD PRINCIPALE ABBIA SALVATO I RISULTATI DELLA PRECEDENTE ITERAZIONE
+        //ASPETTA CHE TUTTI I THREAD ABBIANO TERMINATO L'ITERAZIONE PRECEDENTE
         while (true) {
-            if (canExecute[ID]) {
+            if ( numberExecution >= t*NTHREADS) {
                 break;
             }
         }
 
         //EVOLVE UOLD E SALVA I RISULTATI IN UNEW
         for (int i=start+1; i<end-1; i++) {
-            uNew[(i)%NPOINTS] = uOld[(i)%NPOINTS] + DT*(  -C*((uOld[(i)%NPOINTS]-uOld[(i-1)%NPOINTS])/DX) + K*(uOld[(i+1)%NPOINTS]-2*uOld[i%NPOINTS]+uOld[(i-1)%NPOINTS])/std::pow(DX,2)  );
+            dataStorage[t+1][(i)%NPOINTS] = dataStorage[t][(i)%NPOINTS]
+                + DT*(  -C*((dataStorage[t][(i)%NPOINTS]-dataStorage[t][(i-1)%NPOINTS])/DX)
+                    + K*(dataStorage[t][(i+1)%NPOINTS]-2*dataStorage[t][i%NPOINTS]+dataStorage[t][(i-1)%NPOINTS])/std::pow(DX,2)  );
         }
 
-        // AVVISA IL THREAD PRINCIPALE CHE HA FINITO L'ITERAZIONE MODIFICANDO UNA VARIABILE counter
-        lockCounter.lock();
-        counterThread += 1;
-        canExecute[ID] = false; //STOPPA L'ESECUZIONE FINO A CHE IL THREAD PRINCIPALE NON AVRA' AGGIORNATO I RISULTATI
-        lockCounter.unlock();
-    }
-}
-
-/* QUESTO E' IL THREAD PRINCIPALE --> NE VIENE CREATO SOLO 1
-Il suo scopo è quello di aspettare che tutti i thread abbiano finito di lavorare sulla loro parte di array
-Quando hanno finito (cioè quando counter == NTHREADS) sostituisce uOld con uNew in modo che l'iterazione successiva possa avvenire
-Nel mentre si occupa di risvegliare ogni thread settando canExecute[ID] = true e salva i risultati dell'iterazione in uno storage
-*/
-void dumperThread(double* uOld, const double* uNew, std::vector<double>* storage) {
-
-    for (int t=0; t<ITERATIONS_TIME; t++) {
-
-        // Aspetta che tutti i thread abbiano finito di lavorare su uOld
-        while (true) {
-            if (counterThread == NTHREADS) {
-                break;
-            }
-        }
-
-        // Resetta il contatore
-        lockCounter.lock();
-        counterThread = 0;
-        lockCounter.unlock();
-
-        //Sostituisce unew con uold e salva i risultati nello storage
-        for (int i=0; i<NPOINTS; i++) {
-            uOld[i] = uNew[i];
-            storage[t].push_back(uNew[i]);
-        }
-
-        // RISVEGLIA OGNI THREAD
-        for (int i=0; i<NTHREADS; i++) {
-            canExecute[i] = true;
-        }
+        modifyNumExec.lock();
+        numberExecution += 1; //AVVISA DI AVER EFFETTUATO UNA ITERAZIONE
+        modifyNumExec.unlock();
 
     }
 }
 
 //FUNZIONE PER SALVARE I DATI IN UN FILE CSV
-void writeData (const std::vector<double>* storage, std::string&& name){
+void writeData (std::vector<std::vector<double>>& storage, std::string&& name){
     std::fstream f;
     f.open(name,std::ios::out);
 
     for (int i=0; i<ITERATIONS_TIME; i++) {
         for (int j=0; j<NPOINTS; j++) {
             if (j != NPOINTS-1)
-                f << storage[i].at(j) << ",";
+                f << storage[i][j] << ",";
             else
-                f << storage[i].at(j)<<std::endl;
+                f << storage[i][j]<<std::endl;
         }
     }
     f.close();
@@ -105,41 +73,26 @@ void writeData (const std::vector<double>* storage, std::string&& name){
 //METODO MAIN
 int main() {
 
-    //INIZIALIZZO LE VARIABILI
-    double grid[NPOINTS];
-    double uOld[NPOINTS];
-    double uNew[NPOINTS];
-
-    //RIEMPIO UOLD E UNEW
+    //CREAZIONE DELL'ARRAY DI MEMORIA CONDIVISA IN CUI LAVORERANNO I THREADS
+    std::vector<std::vector<double>> dataStorage(ITERATIONS_TIME, std::vector<double>(NPOINTS));
     for (int i=0; i<NPOINTS; i++) {
-        grid[i] = i*DX;
-        uOld[i]=std::sin(2*M_PI*grid[i]/LENGTH);
-        uNew[i]=uOld[i];
+        dataStorage[0][i]=std::sin(2*M_PI*i*DX/LENGTH);
     }
 
-    // CREO IL THREAD PRINCIPALE
-    std::cout << "INIZIO INTEGRAZIONE" << std::endl;
-    auto* storage = new std::vector<double>[ITERATIONS_TIME];
-    std::thread dump(dumperThread, uOld, uNew, storage);
-
-    //CREO UN NUMERO NTHREADS DI THREADS "LAVORATORI"
+    //CREO UN NUMERO NTHREADS DI THREADS 
     std::vector<std::thread> threadPool;
     for (int i=0; i<NTHREADS; i++) {
-        canExecute[i] = true;
         int start = i*(NPOINTS_THREAD-2); //OGNI THREAD LAVORA SOLO SU UNA FETTA DELL'ARRAY
         int end = (i+1)*(NPOINTS_THREAD+2);
-        threadPool.push_back(std::thread(evolveThread,i,start,end,uNew,uOld));
+        threadPool.push_back(std::thread(evolveThread,start,end,std::ref(dataStorage))); //INIZIO DEL PROCESSO
     }
 
     for(int i=0; i<NTHREADS; i++) {
-        threadPool.at(i).join();       //ASPETTIAMO LA FINE DEL PROCESSO
+        threadPool.at(i).join();       //TERMINE DEL PROCESSO
     }
 
-    dump.join(); //ASPETTIAMO CHE ANCHE IL THREAD PRINCIPALE ABBIA FINITO
-    std::cout << "FINE INTEGRAZIONE" << std::endl;
     //DUMP TO FILE
-    writeData(storage,"dataFile.csv");
-    delete[] storage;
+    writeData(dataStorage,"dataFile.csv");
 
     //POST PROCESSING
     std::system("python3 postProcessorDiffusionConvection.py");
