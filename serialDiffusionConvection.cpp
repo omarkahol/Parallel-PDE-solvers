18bdde701cbfab79ba99fe187abd00466aa06308
omarkahol <okahol@yahoo.it>
2020-02-11 10:42:05 +0100

    modifiche generali

--- serialDiffusionConvection.cpp
+++ serialDiffusionConvection.cpp
@@ -1,91 +1,32 @@
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include <fstream>
 #include <cstring>
 #include <chrono>
+#include "functions.h"
 
-const int N_POINTS = 72;
+const int NPOINTS = 70;
 const double LENGTH = 1;
-const double DX = LENGTH/(N_POINTS-1);
+const double DX = LENGTH/(NPOINTS-1);
 const double SIGMA = 0.1;
 const double DT = SIGMA*DX;
 const int ITERATIONS_TIME=1000;
-const double C = 5;
-const double K = .001;
-
-std::vector<double> getGrid(){
-    std::vector<double> grid(N_POINTS);
-    double start = 0;
-    for (int i=0; i<N_POINTS; i++){
-        grid.at(i)=start;
-        start += DX;
-    }
-    return grid;
-}
-
-std::vector<double> initialCondition(const std::vector<double>& grid) {
-    std::vector<double> initialCond(N_POINTS);
-    for (int i=0; i<N_POINTS; i++){
-        initialCond.at(i)=std::sin(2*M_PI*grid.at(i));
-    }
-    return initialCond;
-}
-
-void writeData (const std::vector<double>* storage, std::string&& name){
-    std::fstream f;
-    f.open(name,std::ios::out);
-
-    for (int i=0; i<ITERATIONS_TIME; i++) {
-        for (int j=0; j<N_POINTS; j++) {
-            if (j != N_POINTS-1)
-                f << storage[i].at(j) << ",";
-            else
-                f << storage[i].at(j)<<std::endl;
-        }
-    }
-    f.close();
-}
 
 int main() {
 
-    // GENERARE UNA GRIGLIA E LA CONDIZIONE INIZIALE
-    auto grid=getGrid();
-    auto uOld = initialCondition(grid);
+    double **pDATA_STORAGE = allocateMemory(ITERATIONS_TIME,NPOINTS);
+    auto initialCondition = [DX, LENGTH](int i){
+        return std::sin(2*M_PI*i*DX/LENGTH);
+    };
+    initGrid(pDATA_STORAGE, initialCondition, NPOINTS);
 
-    auto uNew = initialCondition(grid);
-    int it = 0;
-
-    //ALLOCATE SOME MEMORY
-    auto* storage = new std::vector<double>[ITERATIONS_TIME];
     auto start = std::chrono::high_resolution_clock::now();
-    while (it++<ITERATIONS_TIME) {
-        for (int i = 1; i<N_POINTS-1; i++){
-            
-            // EVOLVO LA FUNZIONE ATTRAVERSO Ut + cUx = kUxx
-            uNew.at(i) = uOld.at(i) + DT*(  -C*((uOld.at(i)-uOld.at(i-1))/DX) + K*(uOld.at(i+1)-2*uOld.at(i)+uOld.at(i-1))/std::pow(DX,2)  );
-        }
-
-        //CONDIZIONE AL CONTORNO PERIODICHE Ux(0,t) = Ut(0,t)
-        uNew.at(N_POINTS-1) = 0.5*(uNew.at(1)+uNew.at(N_POINTS-2));
-
-        //CONDIZIONE AL CONTORNO SULLA FUNZIONE U(0,t) = U(1,t)
-        uNew.at(0) = uNew.at(N_POINTS-1);
-
-        storage[it-1] = uNew;
-
-        uOld = uNew;
+    for(int t=0; t<ITERATIONS_TIME-1; t++) {
+        evolvePDE(pDATA_STORAGE, t, NPOINTS, -1, NPOINTS+2, DX, DT);
     }
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     std::cout << "Elapsed Time: " << duration.count()<< " microseconds" << std::endl;
 
-    //DUMP TO FILE
-    writeData(storage,"dataFile.csv");
-
-    //POST PROCESSING
+    writeData(pDATA_STORAGE,ITERATIONS_TIME,NPOINTS,"dataFile.csv");
+    freeMemory(pDATA_STORAGE, ITERATIONS_TIME);
     std::system("python3 postProcessorDiffusionConvection.py");
-    delete[] storage;
-
     return 0;
 }
--- threadDiffusionConvection.cpp
+++ threadDiffusionConvection.cpp
@@ -1,100 +1,63 @@
-#include <iostream>
-#include <cmath>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
 #include <vector>
-#include <fstream>
-#include <cstring>
+#include <thread>
+#include "functions.h"
 
 //VARIABILI DEL SISTEMA
-const int NTHREADS = 4; //NUMERO DI THREADS
-const int NPOINTS_THREAD = 21; //NUMERO DI PUNTI PER OGNI THREAD
+const int NTHREADS = 15; //NUMERO DI THREADS
+const int NPOINTS_THREAD = 6; //NUMERO DI PUNTI PER OGNI THREAD
 const int NPOINTS = NTHREADS*NPOINTS_THREAD - 2*NTHREADS; //NUMERO DI PUNTI TOTALE
 const double LENGTH = 1;
 const double DX = LENGTH/(NPOINTS-1);
 const int ITERATIONS_TIME=1000;
-const double C = 5;
-const double K = .02;
 const double SIGMA = 0.1;
 const double DT = SIGMA*DX;
+bool threadState[NTHREADS]; //True se il thread è in esecuzione False se non lo è
 
-//VARIABILE PER SALVARE IL NUMERO DI ITERAZIONI EFFETTUATE
-long numberExecution = 0;
-std::mutex modifyNumExec;
 
 /*
 THREAD
-Ogni thread lavora e aggiorna lo stesso file: dataStorage. Tuttavia ogni thread si occupa di evolvere solo 
-i punti interni al suo dominio assegnato tramite [start, end]. Alla fine modifica la variabile numberExecution
-per avviasare che l'iterazione è stata completata con successo.
+Ogni thread lavora e aggiorna lo stesso file: pDATA_STORAGE. Tuttavia ogni thread si occupa di evolvere solo 
+i punti interni al suo dominio assegnato tramite [start, end]. Alla fine modifica la variabile indicatrice del suo stato.
+Prima di cominciare la nuova iterzione, attende che i due thread confinanti abbiano terminato le loro.
 */
-void evolveThread(int start, int end, std::vector<std::vector<double>>& dataStorage) {
+void evolveThread(int ID, int start, int end, double **pDATA_STORAGE) {
+
+    int IDbefore = (ID==0)?NTHREADS-1:ID-1;
+    int IDafter = (ID==NTHREADS-1)?0:ID+1;
 
     for(int t = 0; t<ITERATIONS_TIME-1; t++) {
-
-        //ASPETTA CHE TUTTI I THREAD ABBIANO TERMINATO L'ITERAZIONE PRECEDENTE
-        while (true) {
-            if ( numberExecution >= t*NTHREADS) {
-                break;
-            }
-        }
-
-        //EVOLVE UOLD E SALVA I RISULTATI IN UNEW
-        for (int i=start+1; i<end-1; i++) {
-            dataStorage[t+1][(i)%NPOINTS] = dataStorage[t][(i)%NPOINTS]
-                + DT*(  -C*((dataStorage[t][(i)%NPOINTS]-dataStorage[t][(i-1)%NPOINTS])/DX)
-                    + K*(dataStorage[t][(i+1)%NPOINTS]-2*dataStorage[t][i%NPOINTS]+dataStorage[t][(i-1)%NPOINTS])/std::pow(DX,2)  );
-        }
-
-        modifyNumExec.lock();
-        numberExecution += 1; //AVVISA DI AVER EFFETTUATO UNA ITERAZIONE
-        modifyNumExec.unlock();
-
+        threadState[ID]=true;
+        evolvePDE(pDATA_STORAGE, t, NPOINTS, start, end, DX, DT);
+        threadState[ID] = false;
+        while(threadState[IDbefore] || threadState[IDafter]) {}
     }
 }
 
-//FUNZIONE PER SALVARE I DATI IN UN FILE CSV
-void writeData (std::vector<std::vector<double>>& storage, std::string&& name){
-    std::fstream f;
-    f.open(name,std::ios::out);
-
-    for (int i=0; i<ITERATIONS_TIME; i++) {
-        for (int j=0; j<NPOINTS; j++) {
-            if (j != NPOINTS-1)
-                f << storage[i][j] << ",";
-            else
-                f << storage[i][j]<<std::endl;
-        }
-    }
-    f.close();
-}
-
-//METODO MAIN
 int main() {
 
-    //CREAZIONE DELL'ARRAY DI MEMORIA CONDIVISA IN CUI LAVORERANNO I THREADS
-    std::vector<std::vector<double>> dataStorage(ITERATIONS_TIME, std::vector<double>(NPOINTS));
-    for (int i=0; i<NPOINTS; i++) {
-        dataStorage[0][i]=std::sin(2*M_PI*i*DX/LENGTH);
-    }
+    //RICHIEDE MEMORIA E INIZIALIZZA L'ARRAY
+    double **pDATA_STORAGE = allocateMemory(ITERATIONS_TIME, NPOINTS);
+    auto initialCondition = [DX, LENGTH](int i){
+        return std::sin(2*M_PI*i*DX/LENGTH);
+    };
+    initGrid(pDATA_STORAGE, initialCondition, NPOINTS);
 
     //CREO UN NUMERO NTHREADS DI THREADS 
     std::vector<std::thread> threadPool;
     for (int i=0; i<NTHREADS; i++) {
         int start = i*(NPOINTS_THREAD-2); //OGNI THREAD LAVORA SOLO SU UNA FETTA DELL'ARRAY
         int end = (i+1)*(NPOINTS_THREAD+2);
-        threadPool.push_back(std::thread(evolveThread,start,end,std::ref(dataStorage))); //INIZIO DEL PROCESSO
+        threadPool.push_back(std::thread(evolveThread,i, start, end, pDATA_STORAGE)); //INIZIO DEL PROCESSO
     }
 
+    //ATTENDO CHE OGNI THREAD ABBIA TERMINATO
     for(int i=0; i<NTHREADS; i++) {
         threadPool.at(i).join();       //TERMINE DEL PROCESSO
     }
 
-    //DUMP TO FILE
-    writeData(dataStorage,"dataFile.csv");
-
-    //POST PROCESSING
+    //POSTPROCESSORE
+    writeData(pDATA_STORAGE,ITERATIONS_TIME,NPOINTS,"dataFile.csv");
+    freeMemory(pDATA_STORAGE, ITERATIONS_TIME);
     std::system("python3 postProcessorDiffusionConvection.py");
     return 0;
 }
